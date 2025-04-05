4e553383f71b05b3c1d2622c4f99e9b57562cfba
Aditya Saxena <adityasaxena1234@gmail.com>
2025-04-05 17:42:51 +0530

    change

--- config/site.ts
+++ config/site.ts
@@ -9,6 +9,10 @@ export const siteConfig = {
     github: "https://github.com/adityasaxena-crypto",
   },
   ogImage: "https://asset.cloudinary.com/dzmfq22kt/c0ac36e82d2a2a080c6675e21ef49464",
+  iconIco:
+    "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357384/assets/naman-favicon.ico",
+  logoIcon:
+    "https://res.cloudinary.com/dzmfq22kt/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1743854860/samples/coffee.jpg",
   keywords: [
     "Aditya Saxena",
     "Aditya Saxena Portfolio",
