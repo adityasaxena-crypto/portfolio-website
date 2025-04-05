624f1580b11b745ecf71e5a91fa11fb8a6184259
Aditya Saxena <adityasaxena1234@gmail.com>
2025-04-05 17:00:01 +0530

    change

--- config/site.ts
+++ config/site.ts
@@ -1,19 +1,14 @@
 export const siteConfig = {
   name: "Aditya Saxena Portfolio",
   authorName: "Aditya Saxena",
-  username: "namanbarkiya",
+  username: "aditya.saxena",
   description:
     "Aditya Saxena is a Full Stack Developer, who loves to build web applications and is passionate about learning new technologies.",
   url: "https://nbarkiya.xyz",
   links: {
-    twitter: "https://x.com/namanbarkiya",
-    github: "https://github.com/namanbarkiya",
+    github: "https://github.com/adityasaxena-crypto",
   },
-  ogImage: "https://nbarkiya.xyz/naman-img.jpg",
-  iconIco:
-    "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357384/assets/naman-favicon.ico",
-  logoIcon:
-    "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357294/assets/naman-logo.png",
+  ogImage: "aditya-img.jpg",
   keywords: [
     "Aditya Saxena",
     "Aditya Saxena Portfolio",
--- config/socials.ts
+++ config/socials.ts
@@ -10,26 +10,20 @@ interface SocialInterface {
 export const SocialLinks: SocialInterface[] = [
   {
     name: "Github",
-    username: "@namanbarkiya",
+    username: "@adityasaxena-crypto",
     icon: Icons.gitHub,
-    link: "https://github.com/namanbarkiya",
+    link: "https://github.com/adityasaxena-crypto",
   },
   {
     name: "LinkedIn",
     username: "Aditya Saxena",
     icon: Icons.linkedin,
-    link: "https://www.linkedin.com/in/namanbarkiya",
-  },
-  {
-    name: "Twitter",
-    username: "@namanbarkiya",
-    icon: Icons.twitter,
-    link: "https://twitter.com/namanbarkiya",
+    link: "https://www.linkedin.com/in/aditya-saxena-002878268/",
   },
   {
     name: "Gmail",
-    username: "naman.barkiya02",
+    username: "adityaarchie1234",
     icon: Icons.gmail,
-    link: "mailto:naman.barkiya02@gmail.com",
+    link: "mailto:adityaarchie1234@gmail.com",
   },
 ];
